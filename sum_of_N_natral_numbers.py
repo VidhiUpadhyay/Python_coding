787e2756206a7c347d693f3068e513f27635cf0a
VidhiUpadhyay <vidhi95upadhyay@gmail.com>
2025-09-09 08:36:58 -0700

    Sum of n natural numbers

--- sum_of_N_natral_numbers.py
+++ sum_of_N_natral_numbers.py
@@ -1,1 +1,9 @@
+def sum_of_n_natural_numbers(n):
+    total=0
+    for i in range(n+1):
+        total+=i
+    return total
 
+if __name__=="__main__":
+    n = int(input("Enter a number n to find sum of n natural numbers:"))
+    print(sum_of_n_natural_numbers(n))
